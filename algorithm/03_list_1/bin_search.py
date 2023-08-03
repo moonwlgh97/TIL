a02746a93ffa9ecbe1e0908f7daeb42fd8c66fd6
문지호 <moonwlgh@naver.com>
2023-08-03 18:00:28 +0900

    0803

---
+++ algorithm/03_list_1/bin_search.py
@@ -0,0 +1,20 @@
+#arr : 원본배열
+# N: 배열의 길이
+# key : 타겟
+
+def binary_search(arr,N,key):
+    start = 0
+    end = N-1 # 끝인덱스
+    while start <= end: # 시작지점이 끝 지점보다 작거나 같은 동안
+    mid= (start +end)//2 # 중앙인덱스
+
+
+
+
+
+
+
+numbers = list(range(1, 30, 2))
+print(numbers)
+N = len(numbers)
+target =25
