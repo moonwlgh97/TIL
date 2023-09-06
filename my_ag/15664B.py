11c26b14852a31fe41d2aefa0bf30b6613651c3f
문지호 <moonwlgh@gmail.com>
2023-09-06 22:00:27 +0900

    0906ㅙㅡ

---
+++ my_ag/15664B.py
@@ -0,0 +1,27 @@
+n, m = map(int,input().split())
+li= list(map(int,input().split()))
+li.sort()
+stack=[]
+
+def back():
+    if len(stack)==m:
+        for i in stack:
+            print(li[i],end=' ')
+        print()    
+        return
+    pre =0
+    for i in range(len(li)):
+        if not stack and pre != li[i]:
+            stack.append(i)
+            pre = li[i]
+            back()
+            stack.pop()
+        else:
+            if  i not in stack and pre != li[i] and li[stack[-1]]<=li[i]:
+                stack.append(i)
+                pre=li[i]
+                back()
+                stack.pop()
+
+back()
+
