90f978b8c84af97ea36f4b11d85d1ea12df03d3e
문지호 <moonwlgh@naver.com>
2023-09-07 17:57:01 +0900

    0907

--- my_ag/15663B.py
+++ my_ag/15663B.py
@@ -1,24 +1,27 @@
 n ,m = map(int, input().split())
-li=list((map(int, input().split())))
+li=list(map(int,input().split()))
 li.sort()
 stack=[]
 
-
 def back():
-
-    if len(stack) == m:
+    if len(stack) ==m:
         for i in stack:
-            print(li[i],end=' ')
+            print(li[i], end=' ')
         print()
         return
-    pre = 0
+    pre=0
     for i in range(len(li)):
-        if i not in stack and pre != li[i]:
+        if not stack and pre != li[i]:
             stack.append(i)
             pre=li[i]
             back()
             stack.pop()
 
 
+        if i not in stack and pre != li[i] and li[stack[-1]] <= li[i]:
+            stack.append(i)
+            pre=li[i]
+            back()
+            stack.pop()
 
 back()
